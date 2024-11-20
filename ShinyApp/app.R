d1e9bd815c5e8f4cc758e470af42dd375e534dc3
kjm6 <kjm6@student.ubc.ca>
2024-11-20 13:41:55 -0800

    Updated data and code

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,57 +1,57 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(tidyverse)
 
-deathSeriesAnnual <- read.delim("C:/Users/katie/Downloads/DeathSeriesAnnual.tab")
+# Load in data NEED TO SWITCH THIS
+dataNCHS <- read.csv("C:/Users/katie/Downloads/NCHS_-_Drug_Poisoning_Mortality_by_State__United_States_20241120 (1).csv")
 
 # Define UI for application that draws a graph
 ui <- fluidPage(
-  titlePanel("US Opioid Deaths 1999 - 2021"), 
+  titlePanel("US Overdose Mortality 1999 - 2022"), 
   sidebarLayout(
     sidebarPanel(
-      selectInput("locationInput", "Location",
-                  choices = c("Division 1: New England", 
-                              "Division 2: Middle Atlantic", 
-                              "Division 3: East North Central", 
-                              "Division 4: West North Central", 
-                              "Division 5: South Atlantic", 
-                              "Division 6: East South Central", 
-                              "Division 7: West South Central", 
-                              "Division 8: Mountain", 
-                              "Division 9: Pacific")
-                  ),
+      selectInput("stateInput", "State",
+                  choices = c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+                              "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana",
+                              "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+                              "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",
+                              "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
+                              "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+                              "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+                              "West Virginia", "Wisconsin", "Wyoming", "United States")
+      ),
+      selectInput("sexInput", "Sex",
+                  choices = c("Both Sexes", "Male", "Female")
+      ),
       selectInput("ageInput", "Age",
-                  choices = c("0+","18+", "0-44", "45-64")
-                  ),
+                  choices = c("0-14", "15-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75+", "All Ages")
+      ),
+      selectInput("raceInput", "Race",
+                  choices = c("All Races - All Origins", "Hispanic", "Non-Hispanic Black", "Non-Hispanic White")
+      )
     ),
     mainPanel(
-      plotOutput("plot"),
+      plotOutput("plot")
     )
   )
 )
 
 # Define server logic required to draw a graph
 server <- function(input, output) {
-    output$plot <- renderPlot({
-      filtered <- deathSeriesAnnual %>% 
-        filter(state == input$locationInput,
-               age == input$ageInput,
-               ucdtitle == "Drug",
-               mcdtitle == "Opioid",
-               race != "All",
-               gender == "All"
-               )
-        ggplot(filtered, aes(x = year, y = deaths, color = race)) +
-        geom_point()
-    })
+  output$plot <- renderPlot({
+    # Filter the dataset based on user inputs
+    filtered <- dataNCHS %>%
+      filter(State == input$stateInput,
+             Race.and.Hispanic.Origin == input$raceInput,
+             Sex == input$sexInput,
+             Age.Group == input$ageInput)
+    
+    # Plot the data
+    ggplot(filtered, aes(x = Year, y = Deaths)) +
+      geom_line() +
+      labs(title = paste("Opioid Deaths in", input$stateInput),
+           x = "Year", y = "Deaths") +
+      theme_minimal()
+  })
 }
 
 # Run the application 
