be594754e7ab1e9978d8d51c8ed221402f794196
kjm6 <kjm6@student.ubc.ca>
2024-11-20 11:35:31 -0800

    Uploaded US opiod deaths data

--- VanTreesApp/app.R
+++ VanTreesApp/app.R
@@ -8,42 +8,26 @@
 #
 
 library(shiny)
+library(datateachr)
+library(tidyverse)
+
+deathSeriesAnnual <- read.delim("C:/Users/katie/Downloads/DeathSeriesAnnual.tab")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  plotOutput("id_histogram"),
+  tableOutput("id_table")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$id_histogram <- renderPlot({
+        ggplot(deathSeriesAnnual, aes(x = year, y = deaths)) +
+        geom_point()
+    })
+    output$id_table <- renderTable({
+      deathSeriesAnnual
     })
 }
 
